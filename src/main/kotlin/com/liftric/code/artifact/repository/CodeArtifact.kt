3fd091aa1b873322ea94d2c3b5c09d06abc244e7
Marcel Kesselring <2410681-Ingwersaft@users.noreply.gitlab.com>
2022-07-05 12:17:43 +0200

    feat: support multiple codeartifact endpoints

---
+++ src/main/kotlin/com/liftric/code/artifact/repository/CodeArtifact.kt
@@ -0,0 +1,69 @@
+package com.liftric.code.artifact.repository
+
+import org.gradle.api.provider.Property
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.codeartifact.CodeartifactClient
+import software.amazon.awssdk.services.codeartifact.model.GetAuthorizationTokenResponse
+import software.amazon.awssdk.services.codeartifact.model.GetRepositoryEndpointResponse
+import software.amazon.awssdk.services.sts.StsClient
+
+/**
+ * internal class handling all aws client config and calls
+ */
+abstract class CodeArtifact {
+    abstract val region: Property<Region>
+    abstract val profile: Property<String>
+    abstract val tokenExpiresIn: Property<Long>
+
+    private val stsClient by lazy {
+        StsClient.builder().apply {
+            region.orNull?.let {
+                region(it)
+            }
+            profile.orNull?.let {
+                credentialsProvider {
+                    ProfileCredentialsProvider.create(profile.get()).resolveCredentials()
+                }
+            }
+        }.build()
+    }
+
+    private val client by lazy {
+        CodeartifactClient.builder().apply {
+            region.orNull?.let {
+                region(it)
+            }
+            profile.orNull?.let {
+                credentialsProvider {
+                    ProfileCredentialsProvider.create(profile.get()).resolveCredentials()
+                }
+            }
+        }.build()
+    }
+
+    private val accountId by lazy {
+        stsClient.getCallerIdentity {}.account()
+    }
+
+    internal fun authorizationTokenResponse(domain: String): GetAuthorizationTokenResponse {
+        return client.getAuthorizationToken {
+            it.domain(domain)
+            it.domainOwner(accountId)
+            it.durationSeconds(tokenExpiresIn.getOrElse(1_800))
+        }
+    }
+
+    internal fun repositoryEndpointResponse(
+        domain: String,
+        repository: String,
+        format: String = "maven"
+    ): GetRepositoryEndpointResponse {
+        return client.getRepositoryEndpoint {
+            it.domain(domain)
+            it.domainOwner(accountId)
+            it.repository(repository)
+            it.format(format)
+        }
+    }
+}
